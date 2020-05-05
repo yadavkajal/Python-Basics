a24efaf6fa5df9b3bf5a95bfcafe76dc7deb0c13
yadavkajal <47444928+yadavkajal@users.noreply.github.com>
2020-05-05 15:46:54 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,14 +1,23 @@
 from tkinter import *
+import pandas as pd
 from tkinter import filedialog
 window=Tk()
 window.title("Automation")
+#To open a file and read that
 def fileopen():
-    file1=filedialog.askopenfile()
-    label=Label(text=file1).pack()
+    #global Inc
+    file1=filedialog.askopenfilename()
+    Inc=pd.read_excel(file1)
+    print(Inc)
 def fileopen1():
-    file2=filedialog.askopenfile()
-    label2=Label(text=file2).pack()
+    file2=filedialog.askopenfilename()
+    Req=pd.read_excel(file2)
+    print(Req)
+def fileopen2():
+    file3=filedialog.askopenfilename()
+    Prb=pd.read_excel(file3)
+    print(Prb)
 button1=Button(text="Select Incident file",width=40,command=fileopen).pack()
 button2=Button(text="SelectRequestFile",width=40,command=fileopen1).pack()
-
+button3=Button(text="SelectProblemFile",width=40,command=fileopen1).pack()
 window.mainloop()
